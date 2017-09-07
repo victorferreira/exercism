f16696cd2df1bdb6e84be2cf6afaaae3b3eb36df
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2017-09-06 21:15:18 -0300

    cleanup

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,7 +4,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        // throw new NotImplementedException("You need to implement this function.");
         if (year % 4  == 0 && year % 100 != 0 || year % 400 ==0)
             return true;
         return false;
