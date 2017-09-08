a37826a011eadb4fc2e084a2b78026e6a2fc525d
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2017-09-07 21:26:45 -0300

    far superior implementation

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -8,10 +8,7 @@ var Year = function(input) {
 };
 
 Year.prototype.isLeap = function() {
-    if(this.year % 4 === 0 && this.year % 100 != 0 || this.year % 400 === 0)
-        return true;
-
-    return false;
+    return (this.year % 4 === 0 && this.year % 100 != 0 || this.year % 400 === 0);
 };
 
 module.exports = Year;
